a01e70cf27db3ea86176cef0c447030caaf7c979
yrkov <yrakovch@gmail.com>
2026-01-13 19:32:30 +0100

    improved style

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, Menu} = require("electron");
 const path = require("path");
 
 function createWindow(){
@@ -10,6 +10,7 @@ function createWindow(){
         }
     });
 
+    Menu.setApplicationMenu(null)
     win.loadFile("index.html");
 }
 
