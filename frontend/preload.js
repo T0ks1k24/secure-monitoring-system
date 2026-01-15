533f532d5d4a33644782a0dfa920644abceb750f
yrkov <yrakovch@gmail.com>
2026-01-15 21:31:55 +0100

    done custom frame

--- frontend/preload.js
+++ frontend/preload.js
@@ -1,8 +1,13 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('versions', {
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron
-  
+})
+
+contextBridge.exposeInMainWorld("windowAPI", {
+  minimize: () => ipcRenderer.send("window:minimize"),
+  maximize: () => ipcRenderer.send("window:maximize"),
+  close: () => ipcRenderer.send("window:close")
 })
